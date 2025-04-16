2361c807066fedb9b19392d1ad90ca8d3d5935f6
Sasha Turchanskiy <turchanskiy777@gmail.com>
2025-04-16 11:22:03 +0300

    added Db to properties started porject first time

---
+++ src/main/java/com/phegondev/AlekEcommerce/AlekEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.phegondev.AlekEcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlekEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlekEcommerceApplication.class, args);
+	}
+
+}
