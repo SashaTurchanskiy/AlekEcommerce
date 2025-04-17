4f1000a28cfd0ca8f87453c79be0885a4039d268
Sasha Turchanskiy <turchanskiy777@gmail.com>
2025-04-17 12:26:00 +0300

    implemented entities and dtos classes

---
+++ src/main/java/com/phegondev/AlekEcommerce/enums/OrderStatus.java
@@ -0,0 +1,12 @@
+package com.phegondev.AlekEcommerce.enums;
+
+public enum OrderStatus {
+    PENDING,
+    DELIVERED,
+    CONFIRMED,
+    CANCELLED,
+    RETURNED,
+    SHIPPED
+
+
+}
---
+++ src/main/java/com/phegondev/AlekEcommerce/enums/UserRole.java
@@ -0,0 +1,6 @@
+package com.phegondev.AlekEcommerce.enums;
+
+public enum UserRole {
+    USER,
+    ADMIN
+}
